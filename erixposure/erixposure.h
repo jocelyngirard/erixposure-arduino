3b8b5b6b1dc7901250dd456a9f43ea5df4a148fb
Jocelyn Girard <jocelyn@smartnsoft.com>
2014-10-02 11:58:49 +0200

    Refactor the code, reworking on the fractionnal shutter speeds computation and cleans-up.

--- erixposure/erixposure.h
+++ erixposure/erixposure.h
@@ -1,5 +1,5 @@
 /*
-** erixposure.h for EriXposure Meter
+ ** erixposure.h for EriXposure Meter
  **
  ** Made by Jocelyn GIRARD
  ** Login   <jocelyn@erioxyde.com>
@@ -10,6 +10,8 @@
 
 #ifndef ERIXPOSURE.H
 #define ERIXPOSURE.H
+
+#define length(array)  (sizeof(array) / sizeof(array[0]))
 
 #define BATTERY_PIN                  0
 #define BATTERY_CELLS_COUNT          1 // The number of cells on lipo: 1 for 1S, 2 for 2S, ...
@@ -29,18 +31,10 @@
 #define OLED_CS                      12
 #define OLED_RESET                   13
 
-enum TimeType
-{
-  OutOfRange,
-  Minutes,
-  Seconds,
-  Fractional
-};
-
-const float apertures[] = { 
-  1.2, 1.4, 1.8, 2, 2.8, 4, 5.6, 8, 11, 16 };
-const float isos[] = { 
-  100, 200, 400, 800, 1600, 3200, 6400 };
+const float apertures[] = { 1.2, 1.4, 1.8, 2, 2.8, 4, 5.6, 8, 11, 16 };
+const float isos[] = { 100, 200, 400, 800, 1600, 3200, 6400 };
+const float shutterSpeeds[] = { 1/2.0, 1/4.0, 1/8.0, 1/15.0, 1/30.0, 1/60.0, 1/125.0, 1/250.0, 1/500.0, 1/1000.0, 1/2000.0, 1/4000.0, 1/8000.0 };
+const char* shutterSpeedTexts[] = { "1/2", "1/4", "1/8", "1/15", "1/30", "1/60", "1/125", "1/250", "1/500", "1/1000", "1/2000", "1/4000", "1/8000" };
 
 #endif
 
--- erixposure/erixposure.ino
+++ erixposure/erixposure.ino
@@ -1,16 +1,35 @@
 /*
-** erixposure.ino for EriXposure Meter
+ ** erixposure.ino for EriXposure Meter
  **
  ** Made by Jocelyn GIRARD
  ** Login   <jocelyn@erioxyde.com>
  **
  ** Started on  Sun Sep 28 2014 Jocelyn GIRARD
- ** Last update Mon Sep 29 19:02:10 2014 Jocelyn GIRARD
+ **
+ ** Based on the work of Kevin Kadooka (http://kadookacameraworks.com/light.html)
+ **
+ ** ---
+ **
+ ** Reference en.wikipedia.org/wiki/Exposure_value
+ ** 
+ ** EV : Exposure Value
+ ** Lux : Illuminance
+ ** A : Aperture (F-number)
+ ** t : Exposure time (Shutter speed)
+ ** 
+ ** Lux = (250 / ISO) * 2^EV
+ ** 2^EV = Lux / (250 / ISO)
+ ** EV = log2(Lux / 2.5) = log(Lux / 2.5) / log(2)
+ ** 
+ ** 2^EV = A^2 / t
+ ** t = A^2 / 2^EV = A^2 / (Lux / (250 / ISO))
+ ** N = sqrt(2^EV * t) = sqrt((Lux / (250 / ISO)) * t)
  */
 
 #include <EEPROM.h>
 #include <SPI.h>
 #include <Wire.h>
+#include <math.h>
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -21,8 +40,6 @@
 
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 Adafruit_TSL2561_Unified luxMeter = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
-
-TimeType timeType; //State of shutter speed value display (fractional, seconds, minutes)
 
 boolean isoMode = 0; //ISO mode state
 boolean save = 0; //Save to EEPROM state
@@ -45,11 +62,11 @@ void setup()
   apertureIndex = EEPROM.read(APERTURE_MEMORY_ADDR);
   isoIndex = EEPROM.read(ISO_MEMORY_ADDR);
 
-  if (apertureIndex > (sizeof(apertures) / sizeof(float) - 1))
+  if (apertureIndex >= length(apertures))
   {
     apertureIndex = 4;
   }
-  if (isoIndex > (sizeof(isos) / sizeof(float) - 1))
+  if (isoIndex >= length(isos))
   {
     isoIndex = 0;
   }
@@ -65,10 +82,129 @@ void setup()
   luxMeter.begin();
 
   checkBatteryVoltage();
-  //refresh();
 }
 
 void loop()
+{
+  handleButtons();
+  computeShutterSpeedAndDisplay();
+}
+
+// Need to be refactor, the voltage management is wrong
+void checkBatteryVoltage()
+{
+  float rawVoltage = analogRead(BATTERY_PIN);
+  float currentVoltage = (rawVoltage * 10 / 1023);
+
+  if (currentVoltage < BATTERY_MIN_VOLTAGE)
+  {
+    display.setCursor(5, 19);
+    display.setTextColor(WHITE);
+    display.print("Low Battery! (");
+    display.print(currentVoltage, 2);
+    display.println("V)");
+  }
+  else
+  {
+    display.setCursor(8, 19);
+    display.setTextColor(WHITE);
+    display.print("Battery OK! (");
+    display.print(currentVoltage, 2);
+    display.println("V)");
+  }
+
+  display.drawRoundRect(2,2,124,28,3,WHITE);
+
+  display.display();
+  display.clearDisplay();
+  delay(1000);
+}
+
+float getLuxValue()
+{
+  sensors_event_t event;
+  luxMeter.getEvent(&event);
+  float lux = event.light;
+  return lux;
+}
+
+void computeShutterSpeedAndDisplay()
+{
+  float luxValue = getLuxValue();
+  int shutterSpeedIndex = 0;
+  float exposureValue = log(luxValue / 2.5) / log(2);
+  float exposureTimeInSeconds = pow(apertures[apertureIndex], 2) / (luxValue / (250 / isos[isoIndex]));
+  
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("f/");
+  display.println(apertures[apertureIndex], 1);
+  
+  // Out of Range management, this happens if the sensor is overloaded or senses no light.
+  if (luxValue == 0) 
+  {
+    display.println("OOR!");
+  }
+  
+  // Minutes management
+  else if (exposureTimeInSeconds >= 60.0)
+  {
+    display.print(exposureTimeInSeconds / 60.0, 1);
+    display.println("m");
+  }
+  
+  // Seconds management 
+  else if ((exposureTimeInSeconds >= 0.75) && (exposureTimeInSeconds < 60))
+  {
+    display.print(exposureTimeInSeconds, 1);
+    display.println("s");
+  }
+  
+  // Fractional time management, we find the nearest standard shutter speed linked to exposure time value
+  else if (exposureTimeInSeconds < 0.75)
+  {
+    float shortestSpeedGap = 1.0;
+    for (int index = 0; index < length(shutterSpeeds); index++)
+    {
+      float shutterSpeed = shutterSpeeds[index];
+      float speedGap = fabs(exposureTimeInSeconds - shutterSpeed);
+      if (speedGap < shortestSpeedGap)
+      {
+        shortestSpeedGap = speedGap;
+        shutterSpeedIndex = index;
+      }
+    }
+    display.println(shutterSpeedTexts[shutterSpeedIndex]);
+  }
+
+  // We draw the separator
+  display.drawLine(73, 0, 73, 32, WHITE);
+
+  // We display the ISO value
+  display.setTextSize(1);
+  display.setCursor(76, 0);
+  display.print("ISO");
+  display.println(isos[isoIndex], 0);
+  
+  // We display the exposure value
+  display.setCursor(76,11);
+  display.print("EV=");
+  display.println(exposureValue, 1);
+  
+  // We display the Lux value
+  display.setCursor(76, 22);
+  display.print(luxValue, 1);
+  display.println("Lx");
+
+  display.display();
+  display.clearDisplay();
+  delay(500);
+}
+
+// TODO : Refactor the buttons management to use the 'Surface Mount Navigation Switch Breakout'
+// (https://www.sparkfun.com/products/8236)
+void handleButtons()
 {
   //  boolean incrementButton = digitalRead(INC_BUTTON_PIN); //Poll for button presses
   //  boolean decrementButton = digitalRead(DEC_BUTTON_PIN);
@@ -217,173 +353,8 @@ void loop()
   //  if (meteringButton == true)                        //If the metering button is pressed, get a new illuminance value.
   //    {
   //      refresh();
-  //    }
-  //  refresh();
-  checkBatteryVoltage();
+  //    } 
 }
 
-void checkBatteryVoltage()
-{
-  float rawVoltage = analogRead(BATTERY_PIN);
-  float currentVoltage = (rawVoltage * 10 / 1023);
 
-  if (currentVoltage < BATTERY_MIN_VOLTAGE)
-  {
-    display.setCursor(5, 19);
-    display.setTextColor(WHITE);
-    display.print("Low Battery! (");
-    display.print(currentVoltage, 2);
-    display.println("V)");
-  }
-  else
-  {
-    display.setCursor(8, 19);
-    display.setTextColor(WHITE);
-    display.print("Battery OK! (");
-    display.print(currentVoltage, 2);
-    display.println("V)");
-  }
-  
-  display.drawRoundRect(2,2,124,28,3,WHITE);
-
-  display.display();
-  display.clearDisplay();
-  delay(1000);
-}
-
-float getLux()
-{
-  sensors_event_t event;
-  luxMeter.getEvent(&event);
-  float lux = event.light;
-  return lux;
-}
-
-void refresh()
-{
-  float luxValue = getLux();
-
-  int timeInFraction;
-
-  float exposureTimeInSeconds = pow(apertures[apertureIndex], 2) * 64 / (luxValue * isos[isoIndex]);
-  if (exposureTimeInSeconds >= 60.0)
-  {
-    timeType = Minutes;
-  }
-  else if (exposureTimeInSeconds < 0.75)
-  {
-    timeType = Fractional;
-    if (exposureTimeInSeconds < 0.000125)
-    {
-      timeType = OutOfRange;
-    }
-    if ((exposureTimeInSeconds <= 0.000188) && (exposureTimeInSeconds > 0.000125))
-    {
-      timeInFraction = 8000;
-    }
-    if ((exposureTimeInSeconds <= 0.000375) && (exposureTimeInSeconds > 0.000188))
-    {
-      timeInFraction = 4000;
-    }
-    if ((exposureTimeInSeconds <= 0.00075) && (exposureTimeInSeconds > 0.000375))
-    {
-      timeInFraction = 2000;
-    }
-    if ((exposureTimeInSeconds <= 0.0015) && (exposureTimeInSeconds > 0.00075))
-    {
-      timeInFraction = 1000;
-    }
-    if ((exposureTimeInSeconds <= 0.003) && (exposureTimeInSeconds > 0.0015))
-    {
-      timeInFraction = 500;
-    }
-    if ((exposureTimeInSeconds <= 0.006) && (exposureTimeInSeconds > 0.003))
-    {
-      timeInFraction = 250;
-    }
-    if ((exposureTimeInSeconds <= 0.012333) && (exposureTimeInSeconds > 0.006))
-    {
-      timeInFraction = 125;
-    }
-    if ((exposureTimeInSeconds <= 0.025) && (exposureTimeInSeconds > 0.012333))
-    {
-      timeInFraction = 60;
-    }
-    if ((exposureTimeInSeconds <= 0.05) && (exposureTimeInSeconds > 0.025))
-    {
-      timeInFraction = 30;
-    }
-    if ((exposureTimeInSeconds <= 0.095833) && (exposureTimeInSeconds > 0.05))
-    {
-      timeInFraction = 15;
-    }
-    if ((exposureTimeInSeconds <= 0.1875) && (exposureTimeInSeconds > 0.095833))
-    {
-      timeInFraction = 8;
-    }
-    if ((exposureTimeInSeconds <= 0.375) && (exposureTimeInSeconds > 0.1875))
-    {
-      timeInFraction = 4;
-    }
-    if ((exposureTimeInSeconds <= 0.75) && (exposureTimeInSeconds > 0.375))
-    {
-      timeInFraction = 2;
-    }
-  }
-  else if ((exposureTimeInSeconds >= 0.75) && (exposureTimeInSeconds < 60))
-  {
-    timeType = Seconds;
-  }
-  if (luxValue == 0) //This happens if the sensor is overloaded or senses no light.
-  {
-    timeType = OutOfRange;
-  }
-
-  Serial.println(timeType);
-
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.print("f/");
-  display.println(apertures[apertureIndex], 1);
-
-  if (timeType == OutOfRange)
-  {
-    display.println("OOR!");
-  }
-  else if (timeType == Minutes)
-  {
-    display.print(exposureTimeInSeconds / 60.0, 1);
-    display.println("m");
-  }
-  else if (timeType == Seconds)
-  {
-    display.print(exposureTimeInSeconds, 1);
-    display.println("s");
-  }
-  else if (timeType == Fractional)
-  {
-    display.print("1/");
-    display.println(timeInFraction);
-  }
-
-  display.println(exposureTimeInSeconds, 3);
-
-  display.drawLine(73, 0, 73, 32, WHITE);
-
-  display.setTextSize(1);
-  display.setCursor(76, 0);
-  display.print("ISO");
-  display.println(isos[isoIndex], 0);
-  display.setCursor(76,11);
-  display.print("EV=");
-  float exposureValue = log(pow(apertures[apertureIndex], 2)) / log(2) + log(1 / exposureTimeInSeconds) / log(2);
-  display.println(floor(exposureValue + 0.5), 1);
-  display.setCursor(76, 22);
-  display.print(luxValue, 1);
-  display.println("Lx");
-
-  display.display();
-  display.clearDisplay();
-}
 
